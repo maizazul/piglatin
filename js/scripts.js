1a794e5f56086a5fe68ef9af4b0489b09cee02e7
Danny Pineda and Sarah Read Brown <sreadbrown@gmail.com>
2016-02-29 19:48:35 -0800

    translate words that begin in vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 var piglatin = function(word) {
-  if (word[0] === "a") {
+  if (word[0] === "a" || "e" || "o" || "u" || "i") {
     return word + "ay" ;
   }
 }
+
+$(document).ready(function() {
+  $("form#piglatin").submit(function(event) {
+    var word = $("input#word").val();
+    var result = piglatin(word);
+
+    $("#translated").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
