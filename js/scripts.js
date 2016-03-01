75ca0f351c7e63ba8b7c75ee515d43e536a35fff
Danny Pineda and Sarah Read Brown <sreadbrown@gmail.com>
2016-02-29 20:46:15 -0800

    work with words that start with two consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 var piglatin = function(word) {
-  if (word[0] === "a" || "e" || "o" || "u" || "i") {
+  var translatedword = word;
+  if (word[0] === "a" || word[0] === "e" || word[0] === "o" || word[0] === "u" || word[0] === "i") {
     return word + "ay" ;
+  }
+
+  else if ((word[0] !== "a" || word[0] !== "e" || word[0] !== "o" || word[0] !== "u" || word[0] !== "i") && (word[1] !== "a" || word[1] !== "e" || word[1] !== "o" || word[1] !== "u" || word[1] !== "i")) {
+    return word.slice(2) + word [0] + word [1] + "ay" ;
+  }
+
+  else {
+    return word.slice(1) + word [0] + "ay" ;
   }
 }
 
@@ -15,3 +24,6 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+
+// var  vowels = ['a','e','i','o','u']
